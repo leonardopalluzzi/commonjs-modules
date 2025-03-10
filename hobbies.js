4ae9c1aade4d61ea897af0d8492a31f26dcc89ae
Leonardo Palluzzi <lp.palluzzi@gmail.com>
2025-03-10 13:34:32 +0100

    add destructuring and obj person function

--- hobbies.js
+++ hobbies.js
@@ -1,3 +1,3 @@
 function hobbies(hobbyOne, hobbyTwo, hobbyThree){
-    return [hobbyOne, hobbyTwo, hobbyThree];
+    return [`${hobbyOne}`, `${hobbyTwo}`, `${hobbyThree}`];
 }
--- people.js
+++ people.js
@@ -1,3 +1,20 @@
+//import
 const names = require('./names.js');
-const hobbies = require('./hobbies.js');
+const userHobbies = require('./hobbies.js');
 
+const hobbiesArr = userHobbies('calcio', 'cucina', 'nuoto');
+
+//destructuring
+const userName = names('Luca', 'Pappolo');
+const {firstName, lastName} = userName;
+
+
+
+function person() {
+    return {
+        fullname: `${firstName} ${lastName}`,
+        hobbies: hobbiesArr
+    }
+}
+
+console.log(person());
